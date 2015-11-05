c8fac51b9cd0df4a2ba1c0f2807eea27a7bdff31
Jeff Posnick <jeffy@google.com>
2015-11-05 11:35:09 -0500

    More refactoring/moving the recipe around.

--- appcache-demo/service-worker.js
+++
@@ -1,7 +0,0 @@
-var matches = location.href.match('manifest=([^&]+)');
-if (matches) {
-  importScripts('../sw-toolbox.js');
-  var manifest = decodeURIComponent(matches[1]);
-  toolbox.options.debug = true;
-  toolbox.appCacheManifest(manifest);
-}
